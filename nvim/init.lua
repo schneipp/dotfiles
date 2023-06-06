86a8ecf2e423589ac6eeb9154a861b2bab3fcfce
Ramon Ott <ramon.ott@news-log.ch>
2023-06-06 15:21:45 +0200

    yo

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,14 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
   vim.cmd [[packadd packer.nvim]]
 end
 require('packer').startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
-  use 'andweeb/presence.nvim' 
-  use {"ggandor/leap.nvim", config = function() require("leap").set_default_keymaps() end}
+  use 'andweeb/presence.nvim'
+  use { "ggandor/leap.nvim", config = function() require("leap").set_default_keymaps() end }
   use { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     requires = {
@@ -43,11 +43,11 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  use 'navarasu/onedark.nvim'               -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
+  use 'tpope/vim-sleuth'                    -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
@@ -55,7 +55,7 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
- -- use { 'github/copilot.vim' }
+  -- use { 'github/copilot.vim' }
   use { "zbirenbaum/copilot.lua" }
   use { 'mhartington/oceanic-next' }
   use { 'tanvirtin/monokai.nvim' }
@@ -212,7 +212,7 @@ require('telescope').setup {
   },
   extensions = {
     ["ui-select"] = {
-      require("telescope.themes").get_dropdown { }
+      require("telescope.themes").get_dropdown {}
     }
   }
 }
@@ -369,7 +369,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'pyright', 'tsserver', 'intelephense' }
+local servers = { 'lua_ls', 'clangd', 'pyright', 'tsserver', 'intelephense' }
 local rt = require("rust-tools")
 rt.setup({
   server = {
@@ -395,22 +395,22 @@ for _, lsp in ipairs(servers) do
     on_attach = on_attach,
     capabilities = capabilities,
     settings = {
-        ['rust-analyzer'] = {
-            cargo = {
-                allFeatures = true,
-            },
-            checkOnSave = {
-                allFeatures = true,
-                command = 'clippy',
-            },
-            procMacro = {
-                ignored = {
-                    ['async-trait'] = { 'async_trait' },
-                    ['napi-derive'] = { 'napi' },
-                    ['async-recursion'] = { 'async_recursion' },
-                },
-            },
+      ['rust-analyzer'] = {
+        cargo = {
+          allFeatures = true,
         },
+        checkOnSave = {
+          allFeatures = true,
+          command = 'clippy',
+        },
+        procMacro = {
+          ignored = {
+            ['async-trait'] = { 'async_trait' },
+            ['napi-derive'] = { 'napi' },
+            ['async-recursion'] = { 'async_recursion' },
+          },
+        },
+      },
     },
   }
 end
@@ -450,74 +450,74 @@ cmp.setup {
   },
 }
 
-require('neoai').setup{
-    -- Below are the default options, feel free to override what you would like changed
-    ui = {
-        output_popup_text = "NeoAI",
-        input_popup_text = "Prompt",
-        width = 30,      -- As percentage eg. 30%
-        output_popup_height = 80, -- As percentage eg. 80%
-        submit = "<Enter>", -- Key binding to submit the prompt
+require('neoai').setup {
+  -- Below are the default options, feel free to override what you would like changed
+  ui = {
+    output_popup_text = "NeoAI",
+    input_popup_text = "Prompt",
+    width = 30,                   -- As percentage eg. 30%
+    output_popup_height = 80,     -- As percentage eg. 80%
+    submit = "<Enter>",           -- Key binding to submit the prompt
+  },
+  models = {
+    {
+      name = "openai",
+      model = "gpt-3.5-turbo",
+      params = nil
     },
-    models = {
-        {
-            name = "openai",
-            model = "gpt-3.5-turbo",
-            params = nil
-        },
-    },
-    register_output = {
-        ["g"] = function(output)
-            return output
-        end,
-        ["c"] = require("neoai.utils").extract_code_snippets,
-    },
-    inject = {
-        cutoff_width = 75,
-    },
-    prompts = {
-        context_prompt = function(context)
-            return "Hey, I'd like to provide some context for future "
-                .. "messages. Here is the code/text that I want to refer "
-                .. "to in our upcoming conversations:\n\n"
-                .. context
-        end,
-    },
-    mappings = {
-        ["select_up"] = "<C-k>",
-        ["select_down"] = "<C-j>",
-    },
-    open_api_key_env = "",
-    shortcuts = {
-        {
-            name = "textify",
-            key = "<leader>as",
-            desc = "fix text with AI",
-            use_context = true,
-            prompt = [[
+  },
+  register_output = {
+    ["g"] = function(output)
+      return output
+    end,
+    ["c"] = require("neoai.utils").extract_code_snippets,
+  },
+  inject = {
+    cutoff_width = 75,
+  },
+  prompts = {
+    context_prompt = function(context)
+      return "Hey, I'd like to provide some context for future "
+          .. "messages. Here is the code/text that I want to refer "
+          .. "to in our upcoming conversations:\n\n"
+          .. context
+    end,
+  },
+  mappings = {
+    ["select_up"] = "<C-k>",
+    ["select_down"] = "<C-j>",
+  },
+  open_api_key_env = "",
+  shortcuts = {
+    {
+      name = "textify",
+      key = "<leader>as",
+      desc = "fix text with AI",
+      use_context = true,
+      prompt = [[
                 Please rewrite the text to make it more readable, clear,
                 concise, and fix any grammatical, punctuation, or spelling
                 errors
             ]],
-            modes = { "v" },
-            strip_function = nil,
-        },
-        {
-            name = "gitcommit",
-            key = "<leader>ag",
-            desc = "generate git commit message",
-            use_context = false,
-            prompt = function ()
-                return [[
+      modes = { "v" },
+      strip_function = nil,
+    },
+    {
+      name = "gitcommit",
+      key = "<leader>ag",
+      desc = "generate git commit message",
+      use_context = false,
+      prompt = function()
+        return [[
                     Using the following git diff generate a consise and
                     clear git commit message, with a short title summary
                     that is 75 characters or less:
                 ]] .. vim.fn.system("git diff --cached")
-            end,
-            modes = { "n" },
-            strip_function = nil,
-        },
+      end,
+      modes = { "n" },
+      strip_function = nil,
     },
+  },
 }
 require("octo").setup()
 
