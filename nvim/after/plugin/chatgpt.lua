a86e56b1a6fe6ee52b1d32086227698c5d2caff5
rams <ramon.ott@news-log.ch>
2023-07-12 16:49:33 +0200

    refactor config

---
+++ nvim/after/plugin/chatgpt.lua
@@ -0,0 +1,6 @@
+
+require('chatgpt').setup({
+  -- your config
+  -- see configuration section below
+  async_api_key_cmd = "pass show chatgpt/key",
+})
---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,46 @@
+
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {}
+    }
+  }
+}
+
+require("telescope").load_extension("ui-select")
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', ':w<CR>', { desc = '[ ] Save file' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+-- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+-- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>fs', require('telescope.builtin').lsp_document_symbols, { desc = '[F]ind [S]ymbols' })
+vim.keymap.set('n', '<leader>ps', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>ft', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>bb', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<leader>lf', ':lua vim.lsp.buf.format()<CR>')
+
