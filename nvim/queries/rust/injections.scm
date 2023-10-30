8993b490ef409d0637001e7989ed8d8ee9028e43
rams <ramon.ott@news-log.ch>
2023-10-30 15:08:28 +0100

    perfection

--- nvim/queries/rust/injections.scm
+++ nvim/queries/rust/injections.scm
@@ -1,13 +1,3 @@
-; (macro_invocation
-;   (scoped_identifier
-;     path: (identifier) @_path (#eq? @_path "sqlx")
-;     name: (identifier) @_name (#eq? @_name "query")
-;     (token_tree
-; 	(raw_string_literal) @sql)
-;     (#offset! @sql 1 0 0 0))
-; )
-
-;;(string_literal) @value (#offset! @sql 0 1 0 -1)
 ([
    (string_literal)
    (raw_string_literal)
