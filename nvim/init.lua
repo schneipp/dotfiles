0068ae2804f0dd3f5a4578b5bb8fd43cfa63a0d7
rams <ramon.ott@news-log.ch>
2024-04-02 13:22:05 +0200

    tweaking the things

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,6 +24,27 @@ require('packer').startup(function(use)
     }
   end
   }
+  -- folke/noice.nvim
+  use { 'folke/noice.nvim', config = function()
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,       -- add a border to hover docs and signature help
+      },
+    })
+  end }
   use { 'lvimuser/lsp-inlayhints.nvim' }
   use { "folke/flash.nvim" }
   -- Treesitter playground
@@ -204,7 +225,7 @@ require('packer').startup(function(use)
     'chipsenkbeil/distant.nvim',
     branch = 'v0.3',
     config = function()
-        require('distant'):setup()
+      require('distant'):setup()
     end
   }
   use "nvim-telescope/telescope-dap.nvim"
@@ -351,5 +372,5 @@ vim.g.netrw_list_hide = '.gitattributes$' -- Hide git files
 
 
 if vim.g.neovide then
-vim.o.guifont = "JetBrainsMonoNL Nerd Font:h15"
+  vim.o.guifont = "JetBrainsMonoNL Nerd Font:h15"
 end
