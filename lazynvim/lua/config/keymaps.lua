ab493180783456b8e198241ff79743380e42fd12
Ramon Ott <ramon@julith.ch>
2025-02-16 21:22:51 +0100

    dotfiles updates

--- lazynvim/lua/config/keymaps.lua
+++ lazynvim/lua/config/keymaps.lua
@@ -4,5 +4,7 @@
 -- local map = LazyVim.safe_keymap_set
 local map = vim.keymap.set
 map("n", "<leader>fw", ":Telescope workspaces<CR>", { desc = "List Workspaces", remap = true })
+map("n", "<leader>bi", ":Telescope buffers<CR>", { desc = "List Buffers", remap = true })
+
 map("v", "<S-j>", ":m '>+1<CR>gv=gv")
 map("v", "<S-k>", ":m '<-2<CR>gv=gv")
